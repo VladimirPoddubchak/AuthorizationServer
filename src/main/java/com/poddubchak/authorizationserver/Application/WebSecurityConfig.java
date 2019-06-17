6d40c7a565dee091d4d02f77e313308351ad3f3c
VladimirPoddubchak <45694858+VladimirPoddubchak@users.noreply.github.com>
2019-06-17 13:06:13 +1000

    Add InMemoryClientDetailsService, JWT, symmetric key.

--- src/main/java/com/poddubchak/authorizationserver/Application/WebSecurityConfig.java
+++ src/main/java/com/poddubchak/authorizationserver/Application/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.poddubchak.authorizationserver.Application;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -13,16 +14,22 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .mvcMatchers("/.well-known/jwks.json").permitAll()
+//                .anyRequest().authenticated();
+//    }
+
     @Bean
     @Override
-    protected UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() {
         return new InMemoryUserDetailsManager(
-                User
-                        .withDefaultPasswordEncoder()
+                User.withDefaultPasswordEncoder()
                         .username("user123")
                         .password("user123")
                         .roles("USER")
-                        .build()
-        );
+                        .build());
     }
 }
